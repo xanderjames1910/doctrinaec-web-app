3573e0f62ec34ed2f81c2f4cdd38973930fcd88d
XanderJames1910 <xanderjames1910@gmail.com>
2019-05-17 00:53:16 -0500

    remove debug_toolbar middleware

--- SocialMedia/settings.py
+++ SocialMedia/settings.py
@@ -37,12 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'debug_toolbar',
     'bootstrap4',
     'users.apps.UsersConfig',
-    # 'groups',
     'newsletters',
-    # 'posts',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'SocialMedia.urls'
