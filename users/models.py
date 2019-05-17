f44ce98f37117c19c593bb622f58d93e71ca588e
XanderJames1910 <xanderjames1910@gmail.com>
2019-05-17 00:45:19 -0500

    remove Pillow

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    # image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     bio = models.TextField(blank=True)
     location = models.CharField(max_length=250, blank=True)
     birth_date = models.DateTimeField(null=True, blank=True)
